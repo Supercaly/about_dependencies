cbf43dec6e0b933908307be1874a63904fdacd8c
supercaly <djonline96@gmail.com>
2020-05-31 18:25:11 +0200

    Add code for CLI

--- bin/generate.dart
+++ bin/generate.dart
@@ -1,6 +1,7 @@
 
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:path/path.dart' as path;
 
 import 'src/code_writer.dart';
@@ -8,17 +9,20 @@ import 'src/dependencies_extractor.dart';
 import 'src/unknown_yaml_file_exception.dart';
 
 void main(List<String> args) async {
-  handleLangFiles();
+  if (_isHelpCommand(args))
+    _printHelperDisplay();
+  else
+    handleLangFiles(_generateOption(args));
 }
 
-Future<void> handleLangFiles() async {
+Future<void> handleLangFiles(GenerateOptions options) async {
   // Extract the dependencies into a list
   try {
     final depList = await DependenciesExtractor().extract();
 
     // Generate dart code from the dependency list
     final outputFile = File(
-      path.join(Directory.current.path, "lib/generated", "dependencies.g.dart"));
+      path.join(Directory.current.path, options.outputDir, options.outputFile));
 
     if (!outputFile.existsSync())
       await outputFile.create(recursive: true);
@@ -33,5 +37,49 @@ ${CodeWriter.write(depList)}
     );
   } on UnknownYamlFileException catch(e) {
     print('\u001b[31m[ERROR] about dependencies: ${e.message}\u001b[0m');
+  }
+}
+
+bool _isHelpCommand(List<String> args) {
+  return args.length == 1 && (args[0] == '--help' || args[0] == '-h');
+}
+
+GenerateOptions _generateOption(List<String> args) {
+  var generateOptions = GenerateOptions();
+  var parser = _generateArgParser(generateOptions);
+  parser.parse(args);
+  return generateOptions;
+}
+
+ArgParser _generateArgParser(GenerateOptions generateOptions) {
+  var parser = ArgParser();
+
+  parser.addOption('output-dir',
+    abbr: 'O',
+    defaultsTo: 'lib/generated',
+    callback: (String x) => generateOptions.outputDir = x,
+    help: 'Output folder stores for the generated file');
+
+  parser.addOption('output-file',
+    abbr: 'o',
+    defaultsTo: 'dependencies.g.dart',
+    callback: (String x) => generateOptions.outputFile = x,
+    help: 'Output file name');
+
+  return parser;
+}
+
+void _printHelperDisplay() {
+  var parser = _generateArgParser(null);
+  print(parser.usage);
+}
+
+class GenerateOptions {
+  String outputDir;
+  String outputFile;
+
+  @override
+  String toString() {
+    return 'outputDir: $outputDir outputFile: $outputFile';
   }
 }
